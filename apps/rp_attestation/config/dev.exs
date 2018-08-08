6f741796f702d636efe8847d1a52ed2379e98122
Dmytro Nasyrov <dmytro@pharosproduction.com>
2018-08-08 12:37:23 +0300

    add new RP address

--- apps/rp_attestation/config/dev.exs
+++ apps/rp_attestation/config/dev.exs
@@ -1,7 +1,7 @@
 use Mix.Config
 
 config :rp_attestation,
-  account_address: "0xdfbc3489041d9c3c728b4179c3c358c143c7e98e",
+  account_address: "0x85725cd64753040abc6a065d7a833854a4adbc22", # "0xdfbc3489041d9c3c728b4179c3c358c143c7e98e",
   ap_endpoint: "https://ap-api-test.kimlic.com/api"
 
 config :logger, level: :debug
--- apps/rp_quorum/config/dev.exs
+++ apps/rp_quorum/config/dev.exs
@@ -1,7 +1,7 @@
 use Mix.Config
 
 config :rp_quorum, 
-  account_address: "0x2fd09b326afe28e5c3b9fa920bb0e094335a22bb"
+  account_address: "0x85725cd64753040abc6a065d7a833854a4adbc22" # "0x2fd09b326afe28e5c3b9fa920bb0e094335a22bb"
 
 config :ethereumex, url: "http://51.144.109.43:22000"
 
