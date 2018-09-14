510e9f69026264b0f1ca742ee112fc1211cd6224
Dmytro Nasyrov <dmytro@pharosproduction.com>
2018-09-14 22:58:07 +0300

    add azure staging blob

--- apps/rp_core/config/prod.exs
+++ apps/rp_core/config/prod.exs
@@ -6,18 +6,10 @@ config :rp_core, RpCore.Repo,
   hostname: "stage-postgresql-rp1.postgres.database.azure.com", # System.get_env("DB_HOSTNAME"),
   pool_size: 10 # System.get_env("DB_POOL") |> Integer.parse |> elem(0)
 
-# config :arc_azure,
-#   container: "rp-blob", # System.get_env("AZURE_CONTAINER"),
-#   cdn_url: "https://stagerpblob.blob.core.windows.net" # System.get_env("AZURE_CDN_URL")
-
-# config :ex_azure,
-#   account: "stagerpblob", # System.get_env("AZURE_ACCOUNT"),
-#   access_key: "65tC8WoorHTSrbp90h/hJf7OXQJtdQMWDVJMoE1Ra5U5L+wLNT2F46o5G4hDMgtYKyP1U5YdRvXqIigKvbohHg==" # System.get_env("AZURE_KEY")
-
 config :arc_azure,
-  container: "rp-blob",
-  cdn_url: "https://blobpharos.blob.core.windows.net"
+  container: "rp-blob", # System.get_env("AZURE_CONTAINER"),
+  cdn_url: "https://stagerpblob.blob.core.windows.net" # System.get_env("AZURE_CDN_URL")
 
 config :ex_azure,
-  account: "blobpharos",
-  access_key: "nlXVNa5YkfiYa9UdqMPRb4WdfjqfB5u4KWh0mWLoPxAkxMlSxxUDyqrG3mGrUcQHdTUboG8JTUjGL0BJwwvdDA=="
+  account: "stagerpblob", # System.get_env("AZURE_ACCOUNT"),
+  access_key: "65tC8WoorHTSrbp90h/hJf7OXQJtdQMWDVJMoE1Ra5U5L+wLNT2F46o5G4hDMgtYKyP1U5YdRvXqIigKvbohHg==" # System.get_env("AZURE_KEY")
