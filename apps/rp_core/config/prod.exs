33756430d391778530483ee66cf43f0697dd699a
Dmytro Nasyrov <dmytro@pharosproduction.com>
2018-09-14 15:53:01 +0300

    fix config

--- apps/rp_core/config/prod.exs
+++ apps/rp_core/config/prod.exs
@@ -1,7 +1,6 @@
 use Mix.Config
 
 config :rp_core, RpCore.Repo,
-  database: System.get_env("DB"),
   username: System.get_env("DB_USERNAME"),
   password: System.get_env("DB_PASSWORD"),
   hostname: System.get_env("DB_HOSTNAME"),
