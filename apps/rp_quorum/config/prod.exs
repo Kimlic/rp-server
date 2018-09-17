654f284f352af9f01331ca5b04423b712071a98b
Dmytro Nasyrov <dmytro@pharosproduction.com>
2018-09-17 15:54:10 +0300

    change quorum url

--- apps/rp_quorum/config/prod.exs
+++ apps/rp_quorum/config/prod.exs
@@ -2,4 +2,4 @@ use Mix.Config
 
 config :rp_quorum, account_address: "0x7104d3f287274ff695893350c845633e4369126f" # System.get_env("RELAYING_QUORUM_ADDRESS")
 
-config :ethereumex, url: "http://40.117.78.52:22000" # System.get_env("QUORUM_URL")
+config :ethereumex, url: "http://137.135.110.105:22000" # System.get_env("QUORUM_URL")
