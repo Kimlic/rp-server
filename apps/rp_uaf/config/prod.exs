1190d6b0196052c21a48828d3c2a9410607cc485
Alex Y <39257074+alexsplashex@users.noreply.github.com>
2018-09-20 17:53:50 +0300

    Update prod.exs

--- apps/rp_uaf/config/prod.exs
+++ apps/rp_uaf/config/prod.exs
@@ -1,10 +1,12 @@
 use Mix.Config
 
 config :rp_uaf, RpUaf.Repo,
-  username: "kimlic@stage-postgresql-rp1", # System.get_env("DB_USERNAME"),
+  #username: "kimlic@stage-postgresql-rp1", # System.get_env("DB_USERNAME"),
+  username: "kimlic", # System.get_env("DB_USERNAME"),
   password: "LU6dME4NzQ", # System.get_env("DB_PASSWORD"),
-  hostname: "stage-postgresql-rp1.postgres.database.azure.com", # System.get_env("DB_HOSTNAME"),
-  pool_size: 10 # System.get_env("DB_POOL") |> Integer.parse |> elem(0)
+  #hostname: "stage-postgresql-rp1.postgres.database.azure.com", # System.get_env("DB_HOSTNAME"),
+  hostname: "stage-postgresql-rp1.eastus.cloudapp.azure.com", # System.get_env("DB_HOSTNAME"),
+  pool_size: 45 # System.get_env("DB_POOL") |> Integer.parse |> elem(0)
 
 config :rp_uaf, 
   callback_url: "http://13.68.143.152:4002/api/qr_callback" # System.get_env("RELAYING_CALLBACK_URL")
