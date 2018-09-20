680c60f53355fb4bf966ed502fcbc44d8d35abbd
Alex Y <39257074+alexsplashex@users.noreply.github.com>
2018-09-20 17:54:49 +0300

    Update prod.exs

--- apps/rp_core/config/prod.exs
+++ apps/rp_core/config/prod.exs
@@ -1,10 +1,12 @@
 use Mix.Config
 
 config :rp_core, RpCore.Repo,
-  username: "kimlic@stage-postgresql-rp1", # System.get_env("DB_USERNAME"),
+  #username: "kimlic@stage-postgresql-rp1", # System.get_env("DB_USERNAME"),
+  username: "kimlic", # System.get_env("DB_USERNAME"),
   password: "LU6dME4NzQ", # System.get_env("DB_PASSWORD"),
-  hostname: "stage-postgresql-rp1.postgres.database.azure.com", # System.get_env("DB_HOSTNAME"),
-  pool_size: 10 # System.get_env("DB_POOL") |> Integer.parse |> elem(0)
+  #hostname: "stage-postgresql-rp1.postgres.database.azure.com", # System.get_env("DB_HOSTNAME"),
+  hostname: "stage-postgresql-rp1.eastus.cloudapp.azure.com", # System.get_env("DB_HOSTNAME"),
+  pool_size: 45 # System.get_env("DB_POOL") |> Integer.parse |> elem(0)
 
 config :arc_azure,
   container: "rp-blob", # System.get_env("AZURE_CONTAINER"),
