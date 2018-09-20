6aa4dca706872d08180c23b2272f001364e3c0f7
Alex Y <39257074+alexsplashex@users.noreply.github.com>
2018-09-20 17:57:12 +0300

    Update stage.exs

--- apps/rp_uaf/config/stage.exs
+++ apps/rp_uaf/config/stage.exs
@@ -1,9 +1,11 @@
 use Mix.Config
 
 config :rp_uaf, RpUaf.Repo,
-  username: "kimlic@stage-postgres-pr2", # System.get_env("DB_USERNAME"),
+  #username: "kimlic@stage-postgres-pr2", # System.get_env("DB_USERNAME"),
+  username: "kimlic", # System.get_env("DB_USERNAME"),
   password: "LU6dME4NzQ", # System.get_env("DB_PASSWORD"),
-  hostname: "stage-postgres-pr2.postgres.database.azure.com", # System.get_env("DB_HOSTNAME"),
+  #hostname: "stage-postgres-pr2.postgres.database.azure.com", # System.get_env("DB_HOSTNAME"),
+  hostname: "stage-postgresql.eastus.cloudapp.azure.com", # System.get_env("DB_HOSTNAME"),
   pool_size: 10 # System.get_env("DB_POOL") |> Integer.parse |> elem(0)
 
 config :rp_uaf, 
