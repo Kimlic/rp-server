99ae9d29d70dcdbd20a9040f8350acaca207fa22
Dmytro Nasyrov <dmytro@pharosproduction.com>
2018-10-03 21:25:54 +0300

    more specs

--- apps/rp_http/lib/rp_http/application.ex
+++ apps/rp_http/lib/rp_http/application.ex
@@ -3,10 +3,15 @@ defmodule RpHttp.Application do
 
   use Application
 
+  @timeout 30_000
+
+  ##### Public #####
+
+  @spec start(:normal, list) :: {:ok, pid()}
   def start(_type, _args) do
     children = [
-      :hackney_pool.child_spec(:ap_pool, timeout: 30_000, max_connections: 100),
-      :hackney_pool.child_spec(:kim_pool, timeout: 30_000, max_connections: 5)
+      :hackney_pool.child_spec(:ap_pool, timeout: @timeout, max_connections: 100),
+      :hackney_pool.child_spec(:kim_pool, timeout: @timeout, max_connections: 5)
     ]
     opts = [strategy: :one_for_one, name: RpHttp.Supervisor]
     Supervisor.start_link(children, opts)
