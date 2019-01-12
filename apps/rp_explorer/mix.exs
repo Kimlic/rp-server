e00651bcf4a73588fb236dc4269496ca7ba8ceb0
Dmytro Nasyrov <dmytro@pharosproduction.com>
2019-01-12 12:00:56 +0200

    upgrade blockchain explorer microservice

--- apps/rp_explorer/mix.exs
+++ apps/rp_explorer/mix.exs
@@ -10,21 +10,47 @@ defmodule RpExplorer.MixProject do
       deps_path: "../../deps",
       lockfile: "../../mix.lock",
       elixir: "~> 1.7",
+      build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
+      test_coverage: [tool: ExCoveralls],
+      dialyzer: [
+        plt_add_deps: :transitive,
+        plt_apps: [:erts, :kernel, :stdlib],
+        flags: [
+          "-Wunmatched_returns",
+          "-Werror_handling",
+          "-Wrace_conditions",
+          "-Wunderspecs",
+          "-Wno_opaque"
+        ]
+      ],
       deps: deps()
     ]
   end
 
   def application do
     [
-      extra_applications: [:sasl, :logger, :runtime_tools],
-      mod: {RpExplorer.Application, []}
+      mod: {RpExplorer.Application, []},
+      env: [],
+      registered: [RpServer.Explorer],
+      extra_applications: [
+        :sasl,
+        :logger,
+        :runtime_tools,
+        :observer,
+        :wx
+      ]
     ]
   end
 
   defp deps do
     [
       {:gen_stage, "~> 0.14"}, 
+      {:toml, "~> 0.5"},
+      {:prometheus, "~> 4.2", override: true},
+      {:prometheus_ex, "~> 3.0"},
+      {:prometheus_plugs, "~> 1.1"},
+      {:prometheus_httpd, "~> 2.1"},
 
       {:rp_kimcore, in_umbrella: true},
       {:rp_quorum, in_umbrella: true}
